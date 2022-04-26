2b1ede6e37c8ac0af29c93ccbbdb7f6d4d38b68e
Lalá Nametala <lalanametala@gmail.com>
2022-04-25 23:12:50 -0300

    Updates

---
+++ precache-manifest.08d9146dee99a814d7e545ce5f6ca602.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b192f1badc7df749b1c0148eabbe99fd",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "fe0a328a86fc767e7adf",
+    "url": "/trybewallet/static/css/main.d07cc5ad.chunk.css"
+  },
+  {
+    "revision": "b595464f41e744499d1a",
+    "url": "/trybewallet/static/js/2.66e7f5f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.66e7f5f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe0a328a86fc767e7adf",
+    "url": "/trybewallet/static/js/main.d1114d6c.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  },
+  {
+    "revision": "44103b8a5f365c1bec574884eae8183e",
+    "url": "/trybewallet/static/media/wallet.44103b8a.png"
+  }
+]);
